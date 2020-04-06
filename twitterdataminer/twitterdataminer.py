9b92617ea20e0ff1f965c425754c4005f1d33768
Yannik Eckert <yannik.eckert@gmx.de>
2020-04-07 00:36:42 +0200

    Implemented scraper for the word 'Boris'

--- twitterdataminer/twitterdataminer.py
+++ twitterdataminer/twitterdataminer.py
@@ -1,14 +1,15 @@
 import tweepy
 from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
 import json
-from io import FileIO
 
-json_file = FileIO('../twitter_api_keys.json', mode='r')
-json_object = json.load(json_file)
+with open('../twitter_api_keys.json') as f:
+    json_object = json.load(f)
 
 consumer_key = json_object['api_key']
 consumer_secret = json_object['api_secret_key']
-access_token = json_object['acces_token']
+access_token = json_object['access_token']
 access_secret = json_object['access_token_secret']
 
 auth = OAuthHandler(consumer_key, consumer_secret)
@@ -16,6 +17,21 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    print(status.text)
+
+class MyListener(StreamListener):
+    def on_data(self, raw_data):
+        try:
+            with open('tweets.json', 'a') as f:
+                f.write(raw_data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status_code):
+        print(status_code)
+        return True
+
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['Boris'])
